028662135b16190c0ab4b28fd6a3de1d14b72afb
ELMRABET-Abdelali <78224898+ELMRABET-Abdelali@users.noreply.github.com>
2022-10-02 03:58:57 +0100

    Update helm-ingress-nginx-manual.sh

--- helm-ingress-nginx-manual.sh
+++ helm-ingress-nginx-manual.sh
@@ -1,4 +1,7 @@
 # helm repo add nginx-stable https://helm.nginx.com/stable
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm repo update
-helm install my-release ingress-nginx/ingress-nginx
+# helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+# helm repo update
+# helm install my-release ingress-nginx/ingress-nginx
+
+kubectl create ns ingress-nginx
+helm install myingress ingress-nginx/ingress-nginx -n ingress-nginx --values ./helm-ingress-nginx.yaml
