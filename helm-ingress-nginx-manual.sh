b348eb010f8b2de06fcd2b8e163bd8a678aa3fe7
ELMRABET-Abdelali <78224898+ELMRABET-Abdelali@users.noreply.github.com>
2022-10-02 04:50:17 +0100

    Update helm-ingress-nginx-manual.sh

--- helm-ingress-nginx-manual.sh
+++ helm-ingress-nginx-manual.sh
@@ -4,4 +4,4 @@
 # helm install my-release ingress-nginx/ingress-nginx
 
 kubectl create ns ingress-nginx
-helm install myingress ingress-nginx/ingress-nginx -n ingress-nginx --values ./helm-ingress-nginx.yaml
+helm install myingress ingress-nginx/ingress-nginx -n ingress-nginx --values helm-ingress-nginx.yaml
