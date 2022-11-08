c1e0c0616999f2d2f085002433010948aeba3177
ELMRABET-Abdelali <78224898+ELMRABET-Abdelali@users.noreply.github.com>
2022-11-08 17:45:12 +0100

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,9 @@
+# docker
+apt install docker.io
+
+# docker compose
+apt-get install docker-compose
+
+# curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# chmod +x /usr/local/bin/docker-compose
+# docker-compose --version
